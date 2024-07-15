98fb181ab7d481383321ab8517901623c324b025
Edzer Dionido <jeddionido26@gmail.com>
2024-07-15 10:48:59 +0800

    add new command

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Notifications\EventReminderNotification;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This sends users notifications before event starts';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = \App\Models\Event::with('attendees.user')->whereBetween('start_time', [now(), now()->addDay()])->get();
+
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        $this->info("Found {$eventCount} {$eventLabel}");
+
+        $events->each(fn($event) => $event->attendees->each(
+            fn($attendee) => $this->info($attendee->user->notify(
+                new EventReminderNotification(
+                    $event
+                )
+            ))));
+        $this->info('Reminders finally sent!');
+
+    }
+}
